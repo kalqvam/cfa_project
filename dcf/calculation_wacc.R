784c389e30e1847e580f3a3956f9de6770172011
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-21 00:18:00 +0300

    Update calculation_wacc.R

--- dcf/calculation_wacc.R
+++ dcf/calculation_wacc.R
@@ -6,9 +6,7 @@ calculate_wacc <- function(dcf_inputs) {
   cost_equity <- dcf_inputs$risk_free_rate + dcf_inputs$country_risk_premium + 
     dcf_inputs$market_risk_premium * dcf_inputs$beta
   
-  cost_debt_pretax <- dcf_inputs$risk_free_rate + dcf_inputs$country_risk_premium + 
-    dcf_inputs$credit_spread
-  cost_debt_posttax <- cost_debt_pretax * (1 - dcf_inputs$tax_rate)
+  cost_debt_posttax <- dcf_inputs$cost_debt_pretax * (1 - dcf_inputs$tax_rate)
   
   wacc <- weight_equity * cost_equity + weight_debt * cost_debt_posttax
   
