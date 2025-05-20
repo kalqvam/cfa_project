d32636ada5eb8aaa74ff3fcb7ac07a0497042f51
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-20 19:31:18 +0300

    Update utils.R

--- pro_forma/utils.R
+++ pro_forma/utils.R
@@ -1,23 +1,18 @@
-# Define periods globally
 historical_periods <- c("2021", "2022", "2023", paste0("2024_Q", 1:4))
 projection_periods <- as.character(2025:2030)
 all_periods <- c(historical_periods, projection_periods)
 
-# Key period markers
 historical_end <- "2024_Q3"
 projection_start <- "2024_Q4"
 
-# Business segments
 revenue_regions <- c("Nordics", "Rest_of_Europe", "North_America", "Rest_of_World")
 
-# Financial categories
 expense_categories <- c("Materials_and_services", "Employee_benefits", 
                       "Other_operating_expenses", "Depreciation_and_amortization", 
                       "Net_interest")
 
-# Balance sheet categories
 bs_operating_asset_categories <- c("Intangible_assets", "PP_and_A_owned", 
-                                "PP_and_A`_ROU", "Inventories", 
+                                "PP_and_A_ROU", "Inventories", 
                                 "Trade_receivables", "Other_current_assets",
                                 "Other_non_current_assets")
 
@@ -34,6 +29,8 @@ bs_liability_equity_categories <- c("Reserve_for_invested_equity",
 bs_interest_bearing_debt_categories <- c("Short_term_loans", "Long_term_loans",
                                       "Lease_liabilities_current", 
                                       "Lease_liabilities_non_current")
+
+bs_categories <- names(historical_bs_data)
 
 df_is <- data.frame(Period = all_periods)
 
