3eb3950944818cc534ca4cd033950cae80e62349
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-21 00:17:21 +0300

    Update inputs_dcf.R

--- dcf/inputs_dcf.R
+++ dcf/inputs_dcf.R
@@ -6,7 +6,7 @@ dcf_inputs <- list(
   market_risk_premium = 0.047,
   country_risk_premium = 0,
   beta = 1.3,
-  credit_spread = 0.004,
+  cost_debt_pretax = 0.05,
   
   book_value_equity = 111.5,
   interest_bearing_debt = 37.5,
