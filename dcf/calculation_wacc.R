87143fdd3672c9349edcf91234c31e47f2bc97f7
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-21 00:15:01 +0300

    Update calculation_wacc.R

--- dcf/calculation_wacc.R
+++ dcf/calculation_wacc.R
@@ -3,12 +3,8 @@ calculate_wacc <- function(dcf_inputs) {
   weight_equity <- dcf_inputs$book_value_equity / total_capital
   weight_debt <- dcf_inputs$interest_bearing_debt / total_capital
   
-  de_ratio <- dcf_inputs$interest_bearing_debt / dcf_inputs$book_value_equity
-  leverage_factor <- 1 + (1 - dcf_inputs$tax_rate) * de_ratio
-  levered_beta <- dcf_inputs$unlevered_beta * leverage_factor
-  
   cost_equity <- dcf_inputs$risk_free_rate + dcf_inputs$country_risk_premium + 
-    dcf_inputs$market_risk_premium * levered_beta
+    dcf_inputs$market_risk_premium * dcf_inputs$beta
   
   cost_debt_pretax <- dcf_inputs$risk_free_rate + dcf_inputs$country_risk_premium + 
     dcf_inputs$credit_spread
@@ -21,7 +17,7 @@ calculate_wacc <- function(dcf_inputs) {
     weight_equity = weight_equity,
     weight_debt = weight_debt,
     cost_equity = cost_equity,
-    cost_debt_posttax = cost_debt_posttax,
-    levered_beta = levered_beta
+    beta = beta,
+    cost_debt_posttax = cost_debt_posttax
   ))
 }
