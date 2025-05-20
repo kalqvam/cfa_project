7e1d51a5e924c65c5af8538f6291fa98d6b22a02
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-21 00:23:34 +0300

    Update run_model.R

--- execution/run_model.R
+++ execution/run_model.R
@@ -5,7 +5,7 @@ financial_statements <- run_complete_financial_model(df_is, df_bs)
 income_statement <- financial_statements$income_statement
 balance_sheet <- financial_statements$balance_sheet
 
-wacc_reuslts <- calculate_wacc(dcf_inputs)
-dcf_results <- calculate_dcf(income_statement, balance_sheet, wacc_reuslts, fixed_assets_params)
+wacc_results <- calculate_wacc(dcf_inputs)
+dcf_results <- calculate_dcf(income_statement, balance_sheet, wacc_results, fixed_assets_params)
 
 cat("\nFinancial model executed successfully.\n")
