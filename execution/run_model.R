9c07e9b96c551fa93d500fcd7a68ed55b8928297
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-20 22:31:51 +0300

    Create run_model.R

---
+++ execution/run_model.R
@@ -0,0 +1,8 @@
+source("load_functions.R")
+
+financial_statements <- run_complete_financial_model(df_is, df_bs)
+
+income_statement <- financial_statements$income_statement
+balance_sheet <- financial_statements$balance_sheet
+
+cat("\nFinancial model executed successfully.\n")
