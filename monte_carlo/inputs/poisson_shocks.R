e3709bdbf0557d0e6b1ec88b1127fa37406a65ed
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-24 16:25:25 +0300

    Update poisson_shocks.R

--- monte_carlo/inputs/poisson_shocks.R
+++ monte_carlo/inputs/poisson_shocks.R
@@ -1,3 +1,9 @@
+# monte_carlo/inputs/poisson_shocks.R
+
+# Source the distribution creation function
+source("monte_carlo/inputs/function_ini.R")
+
+# Consolidated shock severity configurations
 shock_severity_configs <- list(
   eu_policy = list(
     levels = c(0.9, 0.75, 0.5, 0.2),
@@ -17,8 +23,33 @@ shock_severity_configs <- list(
   )
 )
 
+# Simplified shock severity function generator
 create_shock_severity_function <- function(config) {
   function() {
     sample(config$levels, 1, prob = config$probabilities)
   }
 }
+
+# Main shock scenarios configuration
+shock_scenarios <- list(
+  eu_policy_shock = list(
+    regions = c("Nordics", "Rest_of_Europe"),
+    shock_distribution = create_poisson_distribution(lambda = 0.15),
+    shock_severity = create_shock_severity_function(shock_severity_configs$eu_policy)
+  ),
+  na_policy_shock = list(
+    regions = c("North_America"),
+    shock_distribution = create_poisson_distribution(lambda = 0.25),
+    shock_severity = create_shock_severity_function(shock_severity_configs$na_policy)
+  ),
+  na_competition_shock = list(
+    regions = c("North_America"),
+    shock_distribution = create_poisson_distribution(lambda = 0.3),
+    shock_severity = create_shock_severity_function(shock_severity_configs$na_competition)
+  ),
+  tech_advancement_shock = list(
+    regions = c("Nordics", "Rest_of_Europe", "North_America", "Rest_of_World"),
+    shock_distribution = create_poisson_distribution(lambda = 0.5),
+    shock_severity = create_shock_severity_function(shock_severity_configs$tech_advancement)
+  )
+)
